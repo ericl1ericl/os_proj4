5b8ca9f1e5cc51dda6f8ce5512c84b9cd7db8cf2
Gabriel King <gking5@nd.edu>
2018-03-28 16:11:34 -0400

    added find and delete functions to hash map

--- threadedRE.c
+++ threadedRE.c
@@ -1,3 +1,7 @@
+// project 4
+// "uthash" used to implement hash map structure in c
+// http://troydhanson.github.io/uthash/userguide.html used as basis for uthash interface functions
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -38,13 +42,15 @@ void parseHeader(FILE *);
 void usage();
 void welcome();
 void addPacket(uint32_t, char *);
+struct PacketHolder * findPacket (uint32_t);
+void deletePacket (struct PacketHolder *);
 
 int main(int argc, char * argv[]) {
   FILE *fp;
 
   // parse command line arguments	
   for (int i = 1; i < argc; i++) {
-	// make sure the first argument is -level 
+    // make sure the first argument is -level 
     if (!strcmp(argv[i], "-level")) {
       if (i == (argc - 1)) {
         usage();
@@ -65,14 +71,14 @@ int main(int argc, char * argv[]) {
     }
   }
   welcome();
-	
+
   //open the file for reading
-	fp = fopen("Dataset-Small.pcap", "r");
-	parseHeader(fp);	
-	DumpInformation(fp);
-	fclose(fp);
-	
-	return 0;
+  fp = fopen("Dataset-Small.pcap", "r");
+  parseHeader(fp);	
+  DumpInformation(fp);
+  fclose(fp);
+
+  return 0;
 }
 
 // startup function
@@ -97,63 +103,63 @@ void usage() {
 }
 
 void parseHeader(FILE *fp) {
-	//jump through 24 bytes of the header of the pcap file
-	fseek(fp, 24, SEEK_CUR);
-	printf("jumped through the header\n");
+  //jump through 24 bytes of the header of the pcap file
+  fseek(fp, 24, SEEK_CUR);
+  printf("jumped through the header\n");
 }
 
 //fread(pointer to memory, size of element to be read, number of elements, 
 //	the pointer to a FILE object)
 void DumpInformation (FILE *fp) {
-	uint32_t nPacketLength;
-	uint32_t newPacketLength = 0;
-	char theData[2000];
+  uint32_t nPacketLength;
+  uint32_t newPacketLength = 0;
+  char theData[2000];
 
-	while(!feof(fp)) {
-		//skip the ts_sec field
-		fseek(fp, 4, SEEK_CUR);
-	
-		//skip the ts_usec field
-		fseek(fp, 4, SEEK_CUR);
+  while(!feof(fp)) {
+    //skip the ts_sec field
+    fseek(fp, 4, SEEK_CUR);
 
-		//Read the incl_len field --> store in nPacketLength
-		fread(&nPacketLength, 4, 1, fp);
+    //skip the ts_usec field
+    fseek(fp, 4, SEEK_CUR);
 
-		//Skip the orig_len field
-		fseek(fp, 4, SEEK_CUR);
-		
-		//ignore packets less than 128 bytes
-		if (nPacketLength < 128) {
-			printf("skipped: too small\n");	
-			fseek(fp, nPacketLength, SEEK_CUR);
-			
-		}
-		// ignore packets greater than 2400 bytes 
-		else if (nPacketLength > 2400) {
-			printf("skipped: too large");
-			fseek(fp, nPacketLength, SEEK_CUR);
-		}
-		else {
-			printf("Packet length was %d\n", nPacketLength);
-			//store in a data structure somehow
-			
-			//skip the first 52 bytes
-			fseek(fp, 52, SEEK_CUR);
+    //Read the incl_len field --> store in nPacketLength
+    fread(&nPacketLength, 4, 1, fp);
 
-			//store the rest of the packet into theData
-			newPacketLength = nPacketLength - 52;
-			fread(theData, 1, newPacketLength, fp);
-			// make a copy of theData
-			char compHash[2000];
-			strncpy(compHash, theData, sizeof(theData));
-			//compute the hash for theData -- 52 bytes through the end of the packet 
-			uint32_t b = 0, c = 0;
-			hashlittle2(compHash, sizeof(theData), &b, &c);
-			// make a packet struct
-			
-		}	
-		//after these loops, start reading the next packet
-	}
+    //Skip the orig_len field
+    fseek(fp, 4, SEEK_CUR);
+
+    //ignore packets less than 128 bytes
+    if (nPacketLength < 128) {
+      printf("skipped: too small\n");	
+      fseek(fp, nPacketLength, SEEK_CUR);
+
+    }
+    // ignore packets greater than 2400 bytes 
+    else if (nPacketLength > 2400) {
+      printf("skipped: too large");
+      fseek(fp, nPacketLength, SEEK_CUR);
+    }
+    else {
+      printf("Packet length was %d\n", nPacketLength);
+      //store in a data structure somehow
+
+      //skip the first 52 bytes
+      fseek(fp, 52, SEEK_CUR);
+
+      //store the rest of the packet into theData
+      newPacketLength = nPacketLength - 52;
+      fread(theData, 1, newPacketLength, fp);
+      // make a copy of theData
+      char compHash[2000];
+      strncpy(compHash, theData, sizeof(theData));
+      //compute the hash for theData -- 52 bytes through the end of the packet 
+      uint32_t b = 0, c = 0;
+      hashlittle2(compHash, sizeof(theData), &b, &c);
+      // make a packet struct
+
+    }	
+    //after these loops, start reading the next packet
+  }
 }
 
 
@@ -166,4 +172,16 @@ void addPacket(uint32_t hash, char * data) {
     HASH_ADD_INT(packets, hash, s);
   }
   strcpy(s->data, data); 
+}
+
+struct PacketHolder * findPacket (uint32_t hash) {
+  struct PacketHolder *s;
+
+  HASH_FIND_INT(packets, &hash, s);  // s: output pointer
+  return s;
+}
+
+void deletePacket (struct PacketHolder *packet) {
+  HASH_DEL(packets, packet);  // packet: pointer to delete
+  free(packet);
 }
